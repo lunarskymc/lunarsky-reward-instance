7a9ef0f17ad1ed23c8c26716b90e1c272e325aa0
Treppenhaus <53356121+Treppenhaus@users.noreply.github.com>
2022-02-25 02:21:26 +0100

    made loot stage-related dia pickaxe haltbarkeit 4 ab stage 5 glück 3 ab stage 3 dig-speed 6 ab stage 7 mending ab stage 6 -> 20% mending ab stage 7 -> 10% . eisen pickaxe haltbarkeit 4 ab stage 6 glück 3 ab stage 3 dig-speed 6 ab stage 4 mending ab stage 5 -> 20% mending ab stage 7 -> 10% . eisen axt haltbarkeit 4 ab stage 6 glück 3 ab stage 3 (30% auf glück) dig-speed 6 ab stage 5, 5 ab stage 3, darunter max 3 mending überall 10% . dia axt (ab hier komplett chaos wtf) 6/30 chance, ab stage 6 4/30 chance auf haltbarkeit 3; haltbarkeit 4 ab stage 7 33% chance bis stage 4, danach 100% max glück 2 | glück 3 ab stage 6 dig-speed 6 ab stage 8+ 2/43 chance auf mending plscomeback — heute um 02:09 Uhr . items 4-6x netherrite scrap (jeweils 1 bis stage(max4) pro stack) 2-3x dragon-breath (jeweils 2 bis stagelevel(min 5) ab stage 1) stage 9 -> 1% chance auf 120er federfly
    N = 7, ab stage 5 ist N 5
    (also mehr der verzauberten items in höheren stages)
    
    3-Nx phantomhaut (jeweils 4 bis stage*2 pro stack)
    3-Nx dias (jeweils 4 bis 16 pro stack)
    3-Nx goldbarren (jeweils 4 bis 20 pro stack)
    3-Nx smaragde (jeweils 4 bis 9 pro stack)
    3-Nx goldkarotten (jeweils 16 bis 30 pro stack) NUR BIS INKL. STAGE 4
    3-Nx gapple (jeweils 5 bis 30 pro stack) NUR BIS INKL. STAGE 5
    3-Nx prismarinshards (jeweils 5 bis 30 pro stack)
    .
    verzauberte bücher
    jeweils 2-3 stück (alle stages)
    - immer nur eine verzauberung
    
    prot 4 ab stage 4, prot 5 ab stage 6
    sharp 4 ab stage 3, sharp 6 ab stage 8
    mining speed 6 ab stage 8
    33% chance mending (alle stages)
    😵‍💫

--- src/main/java/de/lunarsky/rewardinstance/InstanceLoot.java
+++ src/main/java/de/lunarsky/rewardinstance/InstanceLoot.java
@@ -13,116 +13,169 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InstanceLoot implements CommandExecutor {
-    private static ArrayList<ItemStack> loot;
+    private static HashMap<Integer, ArrayList<ItemStack>> stageLoot;
 
-    public static void fillLoot() {
-        loot = new ArrayList<>();
-        for(int i = 0; i < Helper.randInt(2, 3); i++) {
+    public static void fillLoot(int stage) {
+        ArrayList<ItemStack> loot = new ArrayList<>();
+
+        for(int i = 0; i < Helper.randInt(1, 3); i++) {
             ItemStack pick = new ItemStack(Material.DIAMOND_PICKAXE);
             ItemMeta meta = pick.getItemMeta();
-            pick.addUnsafeEnchantment(Enchantment.DURABILITY, Helper.randInt(1, 4));
-            pick.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, Helper.randInt(1, 2));
-            pick.addUnsafeEnchantment(Enchantment.DIG_SPEED, Helper.randInt(1, 6));
-            if(Helper.randInt(0, 30) <= 2) pick.addUnsafeEnchantment(Enchantment.MENDING, 1);
+            pick.addUnsafeEnchantment(Enchantment.DURABILITY, Helper.randInt(1, stage < 5 ? 3 : 4));
+            pick.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, Helper.randInt(1, stage < 3 ? 1 : 3));
+            pick.addUnsafeEnchantment(Enchantment.DIG_SPEED, Helper.randInt(1, stage < 7 ? 5 : 6));
+            if(Helper.randInt(0, 100) <= (stage > 6 ? 10 : 20) && stage >= 6) pick.addUnsafeEnchantment(Enchantment.MENDING, 1);
             loot.add(pick);
         }
-
-
         for(int i = 0; i < Helper.randInt(2, 5); i++) {
             ItemStack pick = new ItemStack(Material.IRON_PICKAXE);
             ItemMeta meta = pick.getItemMeta();
-            pick.addUnsafeEnchantment(Enchantment.DURABILITY, Helper.randInt(1, 4));
-            pick.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, Helper.randInt(1, 2));
-            pick.addUnsafeEnchantment(Enchantment.DIG_SPEED, Helper.randInt(4, 6));
-            if(Helper.randInt(0, 10) <= 3) pick.addUnsafeEnchantment(Enchantment.MENDING, 1);
+            pick.addUnsafeEnchantment(Enchantment.DURABILITY, Helper.randInt(1, stage < 6 ? 3 : 4));
+            pick.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, Helper.randInt(1, stage < 3 ? 1 : 3));
+            pick.addUnsafeEnchantment(Enchantment.DIG_SPEED, Helper.randInt(1, stage < 4 ? 5 : 6));
+            if(Helper.randInt(0, 100) <= (stage > 6 ? 10 : 20) && stage >= 5) pick.addUnsafeEnchantment(Enchantment.MENDING, 1);
             loot.add(pick);
         }
-
-
         for(int i = 0; i < Helper.randInt(2, 5); i++) {
             ItemStack axe = new ItemStack(Material.IRON_AXE);
             ItemMeta meta = axe.getItemMeta();
-            axe.addUnsafeEnchantment(Enchantment.DURABILITY, Helper.randInt(2, 4));
-            axe.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, Helper.randInt(1, 2));
-            axe.addUnsafeEnchantment(Enchantment.DIG_SPEED, Helper.randInt(4, 6));
-            if(Helper.randInt(0, 10) <= 3) axe.addUnsafeEnchantment(Enchantment.MENDING, 1);
+            axe.addUnsafeEnchantment(Enchantment.DURABILITY, Helper.randInt(2, stage < 6 ? 3 : 4));
+            if(Helper.randInt(0, 10) <= 2) axe.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, Helper.randInt(1, stage < 3 ? 2 : 3));
+            axe.addUnsafeEnchantment(Enchantment.DIG_SPEED, Helper.randInt(4, stage > 4 ? 6 : (stage >= 3 ? 5 : 3)));
+            if(Helper.randInt(0, 10) <= 1) axe.addUnsafeEnchantment(Enchantment.MENDING, 1);
             loot.add(axe);
         }
-
         for(int i = 0; i < Helper.randInt(2, 3); i++) {
             ItemStack axe = new ItemStack(Material.DIAMOND_AXE);
             ItemMeta meta = axe.getItemMeta();
-            axe.addUnsafeEnchantment(Enchantment.DURABILITY, Helper.randInt(1, 4));
-            axe.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, Helper.randInt(1, 2));
-            axe.addUnsafeEnchantment(Enchantment.DIG_SPEED, Helper.randInt(1, 6));
-            if(Helper.randInt(0, 30) <= 2) axe.addUnsafeEnchantment(Enchantment.MENDING, 1);
+            if(Helper.randInt(0, 30) <= (stage > 5 ? 4 : 6)) axe.addUnsafeEnchantment(Enchantment.DURABILITY, Helper.randInt(1, stage > 6 ? 4 : 3));
+            if(stage > 4 || Helper.randInt(0, 10) <= 3) axe.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, Helper.randInt(1, stage > 5 ? 3 : 2));
+            axe.addUnsafeEnchantment(Enchantment.DIG_SPEED, Helper.randInt(1, stage < 7 ? 6 : 5));
+            if(Helper.randInt(0, 43) <= 2) axe.addUnsafeEnchantment(Enchantment.MENDING, 1);
             loot.add(axe);
         }
-
-        for(int i = 0; i < Helper.randInt(4, 5); i++) {
-            loot.add(new ItemStack(Material.NETHERITE_SCRAP, Helper.randInt(1, 3)));
-        }
-
         for(int i = 0; i < Helper.randInt(4, 6); i++) {
-            loot.add(new ItemStack(Material.PHANTOM_MEMBRANE, Helper.randInt(4, 12)));
-            loot.add(new ItemStack(Material.DIAMOND, Helper.randInt(4, 20)));
-            loot.add(new ItemStack(Material.GOLD_INGOT, Helper.randInt(4, 20)));
-            loot.add(new ItemStack(Material.EMERALD, Helper.randInt(4, 20)));
-            loot.add(new ItemStack(Material.GOLDEN_APPLE, Helper.randInt(2, 5)));
-            loot.add(new ItemStack(Material.GOLDEN_CARROT, Helper.randInt(16, 30)));
+            loot.add(new ItemStack(Material.NETHERITE_SCRAP, Helper.randInt(1, Math.min(stage, 4))));
         }
-
+        for(int i = 0; i < Helper.randInt(3, stage < 5 ? 5 : 7); i++) {
+            loot.add(new ItemStack(Material.PHANTOM_MEMBRANE, Helper.randInt(4, stage * 2)));
+            loot.add(new ItemStack(Material.DIAMOND, Helper.randInt(4, 16)));
+            loot.add(new ItemStack(Material.GOLD_INGOT, Helper.randInt(4, 20)));
+            loot.add(new ItemStack(Material.EMERALD, Helper.randInt(4, 9)));
+            if(stage <= 4) loot.add(new ItemStack(Material.GOLDEN_CARROT, Helper.randInt(16, 30)));
+            if(stage <= 5) loot.add(new ItemStack(Material.GOLDEN_APPLE, Helper.randInt(1, 3)));
+            loot.add(new ItemStack(Material.PRISMARINE_SHARD, Helper.randInt(5, 30)));
+        }
         for(int i = 0; i < Helper.randInt(2, 3); i++) {
             ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
             EnchantmentStorageMeta meta = (EnchantmentStorageMeta) book.getItemMeta();
-            meta.addStoredEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, Helper.randInt(1, 5), true);
+            meta.addStoredEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, Helper.randInt(1, stage > 5 ? 5 : stage > 3 ? 4 : 3), true);
             book.setItemMeta(meta);
             loot.add(book);
 
             book = new ItemStack(Material.ENCHANTED_BOOK);
             meta = (EnchantmentStorageMeta) book.getItemMeta();
-            meta.addStoredEnchant(Enchantment.DAMAGE_ALL, Helper.randInt(2, 6), true);
+            meta.addStoredEnchant(Enchantment.DAMAGE_ALL, Helper.randInt(2, Helper.randInt(1, stage > 7 ? 5 : stage > 2 ? 4 : 3)), true);
             book.setItemMeta(meta);
             loot.add(book);
 
             book = new ItemStack(Material.ENCHANTED_BOOK);
             meta = (EnchantmentStorageMeta) book.getItemMeta();
-            meta.addStoredEnchant(Enchantment.DIG_SPEED, Helper.randInt(3, 6), true);
+            meta.addStoredEnchant(Enchantment.DIG_SPEED, Helper.randInt(3, stage > 7 ? 6 : 5), true);
             book.setItemMeta(meta);
             loot.add(book);
 
-            book = new ItemStack(Material.ENCHANTED_BOOK);
-            meta = (EnchantmentStorageMeta) book.getItemMeta();
-            meta.addStoredEnchant(Enchantment.MENDING, 1, true);
-            book.setItemMeta(meta);
-            loot.add(book);
-
-            loot.add(new ItemStack(Material.DRAGON_BREATH, Helper.randInt(2, 6)));
+            if(Helper.randInt(0, 10) <= 3) {
+                book = new ItemStack(Material.ENCHANTED_BOOK);
+                meta = (EnchantmentStorageMeta) book.getItemMeta();
+                meta.addStoredEnchant(Enchantment.MENDING, 1, true);
+                book.setItemMeta(meta);
+                loot.add(book);
+            }
+            loot.add(new ItemStack(Material.DRAGON_BREATH, Helper.randInt(2, Math.max(5, stage))));
         }
+        if(stage > 8 && Helper.randInt(0, 100) < 2) loot.add(createNewFederfly(120));
+        stageLoot.put(stage, loot);
     }
 
-    public static ArrayList<ItemStack> getLoot() {
-        return loot;
+    /**
+     * gets all the loot for a stage as an ArrayList<ItemStack>
+     * @param stage     the stage
+     * @return          the loot
+     */
+    public static ArrayList<ItemStack> getLoot(int stage) {
+        return stageLoot.getOrDefault(stage, new ArrayList<>());
     }
 
     /**
      * -
      * @return      1 random item from loot list (must be filled before)
      */
-    public static ItemStack getRandomItem() {
+    public static ItemStack getRandomItem(int stage) {
+        ArrayList<ItemStack> loot = getLoot(stage);
         return loot.get(Helper.randInt(0, loot.size() - 1));
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        final String syntax = "§c/peekloot <int:stage>";
         if(sender instanceof Player p) {
             if(p.hasPermission("lunarsky.instance.peekloot")) {
-                World w = p.getWorld();
-                getLoot().forEach(item -> w.dropItem(p.getLocation(), item));
+                if(args.length != 1) {
+                    p.sendMessage(syntax);
+                    return false;
+                }
+                try {
+                    World w = p.getWorld();
+                    int stage = Integer.parseInt(args[0]);
+                    getLoot(stage).forEach(item -> w.dropItem(p.getLocation(), item));
+                    p.sendMessage("§aloot gedroppt!");
+                }catch (NumberFormatException e1) {
+                    p.sendMessage("§7\"stage\"§c muss eine Nummer sein!");
+                }
             }
         }
         return false;
+    }
+
+    /**
+     * creates a new federfly item
+     * @param max_uses      max uses
+     * @return              itemstack
+     */
+    public static ItemStack createNewFederfly(int max_uses) {
+        ItemStack item = new ItemStack(Material.FEATHER);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName("§b§lFederfly");
+        meta.setLore(getFederflyLore(max_uses, max_uses));
+        meta.addEnchant(Enchantment.DURABILITY, 1, false);
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
+    /**
+     * just returns the correct federfly lore for each item
+     * @param remaining_uses    remaining uses: int
+     * @param max_uses          max uses : int
+     * @return                  arraylist<stirng> (the lores for the itemstack)
+     */
+    public static ArrayList<String> getFederflyLore(int remaining_uses, int max_uses) {
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add("§7Haltbarkeit: §b{durability_remaining} §7/ §3{durability_max}"
+                .replace("{durability_remaining}", Integer.toString(remaining_uses))
+                .replace("{durability_max}", Integer.toString(max_uses)));
+
+        lore.add("§7Klicke mit dem Item in die Luft,");
+        lore.add("§7um einen Boost zu erhalten");
+        lore.add("§7der dich §bfliegen §7lässt!");
+        lore.add("§7");
+        lore.add("§bDieses Item kann mit Phantomhäuten");
+        lore.add("§bam Amboss repariert werden!");
+
+        return lore;
     }
 }
